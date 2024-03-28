a4d18ee43f634865e83a9da474222c5ef6e27c94
Flex Tex <duttsanjay6589@gmail.com>
2024-03-28 16:24:37 +0530

    add: audio change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import skull from "./skull";
 import InfiniteLoopAudio from "./InfiniteLoopAudio";
-import voice from "./assets/audio/audio.mp3";
+
 function App() {
   return (
     <>
       <skull></skull>
-      <InfiniteLoopAudio src={voice} />
+      <InfiniteLoopAudio src="./assets/audio/audio.mp3" />
     </>
   );
 }
