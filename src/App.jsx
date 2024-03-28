f34b1b12de1c419788626d24958dc36ce10c71af
Flex Tex <duttsanjay6589@gmail.com>
2024-03-28 17:03:29 +0530

    add: mu change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import AudioPlayer from "./AudioPlayer";
-
+import audio from ".assetsaudioaudio.mp3";
 function App() {
   return (
     <>
-      <AudioPlayer src=".\assets\audio\audio.mp3" />
+      <AudioPlayer src={audio} />
     </>
   );
 }
--- src/AudioPlayer.jsx
+++ src/AudioPlayer.jsx
@@ -1,11 +1,16 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 
 const AudioPlayer = ({ src }) => {
   const audioRef = useRef(null);
 
+  useEffect(() => {
+    audioRef.current.play();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Empty dependency array to trigger effect only once when component mounts
+
   return (
     <div>
-      <audio ref={audioRef} src={src} autoPlay />
+      <audio ref={audioRef} src={src} />
     </div>
   );
 };
